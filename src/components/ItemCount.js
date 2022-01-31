ec9b98a3b5c1ec9ec78873d2c09f4cde0d72259f
Joaquin Pastor <joaquinpastorsuiffet@gmail.com>
2022-01-31 15:00:03 -0300

    Add Function onAdd

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -4,6 +4,7 @@ import "./ItemCount.css";
 function ItemCount() {
   const [counter, setCounter] = useState(0);
   // const [msj, setMsj] = useState("Agregado al carrito");
+  const stock = 15;
 
   function addItem() {
     if (stock > 0) {
@@ -13,14 +14,16 @@ function ItemCount() {
     }
   }
 
-  const stock = 0;
-
   function restItem() {
     if (stock > 0) {
       if (counter >= 1) {
         setCounter(counter - 1);
       }
     }
+  }
+
+  function onAdd() {
+    alert(`Usted agrego ${counter} items al carrito`);
   }
 
   return (
@@ -35,9 +38,45 @@ function ItemCount() {
           +
         </button>
       </fieldset>
-      <button className="btnAddToCart">Agregar al carrito</button>
+      <button onClick={onAdd} className="btnAddToCart">
+        Agregar al carrito
+      </button>
     </div>
   );
 }
 
 export default ItemCount;
+
+// //CLASE DE PRMISE Y MAP
+// const productos = [
+//   {
+//     id: 2,
+//     title: "Fogonero 120",
+//     descripcion: "fogonero 120cm diametro",
+//     precio: 25000,
+//   },
+//   {
+//     id: 1,
+//     title: "Fogonero 100",
+//     descripcion: "fogonero 120cm diametro",
+//     precio: 25000,
+//   },
+//   {
+//     id: 3,
+//     title: "Fogonero 75",
+//     descripcion: "fogonero 120cm diametro",
+//     precio: 25000,
+//   },
+// ];
+// const promesa = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve(productos);
+//     // retorne de productos
+//   }, 2000);
+// });
+
+// function getItems() {
+//   console.log(promesa);
+// }
+
+// export { getItems };
